dd079c22d8edc18d9a25b7fa07845862c64193f7
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2023-04-11 18:17:32 +0200

    s25416Commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+
+long_description = "My long description"
+
+setuptools.setup(
+    name="PPY_Poject_2023",
+    version="1.0.0",
+    author="Oleksandr Sokil, Anton Makhniuk",
+    author_email="s25416@pjwstk.edu.pl,",
+    description="Web Media Catalog Service,s24379@pjwsi.edu.pl",
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires=[
+        "nonion==0.4.4",
+    ],
+)
