ae54ae6a89d68fca430aefbb197eb9891f9aec73
OleksandrSokils25416 <127222791+OleksandrSokils25416@users.noreply.github.com>
2023-04-11 19:05:30 +0200

    s25416Commit

---
+++ Module/main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI
+from typing import Optional
+
+app = FastAPI()
+
+#Path parameters
+
+@app.get("/add/{x}/{y}")
+def add(x:float,y:float)-> float:
+    return x+y
+
+#Query paramters
+@app.get("/increment/{x}")
+def increment(x:int,y:int,z:Optional[str] =None)-> int:
+    print(z or "hello!")
+    return x+y
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
